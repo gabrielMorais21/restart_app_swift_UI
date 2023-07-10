b373d1a6f30b27d9c493b6e58c09fac467928e36
gabriel.dias <gabriel.dias@granitopagamentos.com.br>
2023-07-10 17:12:28 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Gabriel Morais Dias on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
